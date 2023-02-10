d456f7a96ad67f6ad76fa9fe552380ce959e0ffa
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-10 12:58:10 +0600

    explore array index

--- array.js
+++ array.js
@@ -1,3 +1,11 @@
 var age = [10, 32, 51, 47, 31, 60];
 
-console.log(age[50])
+//* get the array element using index
+// console.log(age[5]) 
+console.log(age[4])
+
+//* change or add the array element value by index
+age[0] = 100;
+// console.log(age)
+age[6]= 200;
+console.log(age)
---
+++ index.js
@@ -0,0 +1,15 @@
+var arr = [10, 20, 24, 19, 37, 14, 59];
+var team = ['Bangladesh', 'Srilanka', 'Pakistan', 'India', 'Afganistan'];
+console.log(team.indexOf('India'))
+
+//* get the array element by index
+// console.log(arr[6])
+
+//* change or add value by index
+arr[0] = 99;
+arr[7] = 199;
+arr[8] = 200;
+// console.log(arr)
+
+//* find index of an element
+// console.log(arr.indexOf(2000))
