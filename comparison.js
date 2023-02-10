972ecaf534c6612655468222316fad55e6444487
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-10 15:11:26 +0600

    simple comparison oprerators

---
+++ comparison.js
@@ -0,0 +1,23 @@
+//* greater than
+// console.log(5 > 6)
+
+//* less than
+// console.log(4 < 5)
+
+//* equal
+// console.log(5 == '5') //! always use === equal instead of ==
+
+//* not equal
+// console.log(5 !== 6)
+
+//* greater than or equal
+// console.log(5 >= 5)
+
+//* less than or equal
+// console.log(4 <= 3)
+
+//* logical AND operator &&
+// console.log(4 < 5 && 7 >= 6)
+
+//* Logical OR operator ||
+console.log(5 > 4 || 1 < 0)
